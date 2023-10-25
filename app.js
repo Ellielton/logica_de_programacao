c4ac2fb819cbc3541b64c7c3ec5726f5cc9d8d7e
Ellielton <ellieltongama@gmail.com>
2023-10-25 00:02:45 -0300

    Comparando variáveis atraves da estrutura if.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
-prompt('Escolha um número entre 1 e 10');
+//var chute armazena o que o usuário digitar no prompt.
+let chute = prompt('Escolha um número entre 1 e 10');
+
+//condicional que compara se o chute é igual ao número secreto.
+if (chute == numeroSecreto) {
+  console.log('Isso ai! Você descobriu o numero secreto é (5)');
+}
