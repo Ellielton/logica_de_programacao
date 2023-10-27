9b07e618721c133f327d6c89f3ef940078029dfd
Ellielton <ellieltongama@gmail.com>
2023-10-27 18:06:57 -0300

    Na aula anterio foi construido ifs encadeados para adicionas dicas para o usuário. Nesta aula criamos um laço de repetição com o while.

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 4;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
 
 //var chute armazena o que o usuário digitar no prompt.
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
 
-//condicional que compara se o chute é igual ao número secreto.
-if (chute == numeroSecreto) {
- alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto}`);
-} else {
-  alert('Você errou :(');
+//Enquanto o chut não for igual ao numero secreto faça:
+while (chute != numeroSecreto) {
+  chute = prompt('Escolha um número entre 1 e 10');
+  //condicional que compara se o chute é igual ao número secreto.
+  if (chute == numeroSecreto) {
+    
+    alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto}`);
+    
+  } else {
+    if (chute > numeroSecreto) {
+      
+      alert(`O numero secreto e menor que ${chute}`);
+      
+    } else {
+      
+      alert(`O numero secreto e maior que ${chute}`);
+    
+    }
+  }
 }
