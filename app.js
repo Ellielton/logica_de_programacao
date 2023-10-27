83f9dec4731c62fe600f9fe5dc6597d91262f3f7
Ellielton <ellieltongama@gmail.com>
2023-10-27 11:29:10 -0300

    Concatenar é junatar uma variável a kuma string para ser exibida na tela para o usuário, de forma que se o valor da variável seja alterado a mesagem seja atualizada automaticamente evitando perda de tempo. E para isso usamos o conceito de "tamplante string."

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
+console.log(numeroSecreto);
+
 //var chute armazena o que o usuário digitar no prompt.
 let chute = prompt('Escolha um número entre 1 e 10');
 
 //condicional que compara se o chute é igual ao número secreto.
 if (chute == numeroSecreto) {
-  console.log('Isso ai! Você descobriu o numero secreto é (5)');
+ alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto}`);
 } else {
-  alert('Você errou :(')
+  alert('Você errou :(');
 }
