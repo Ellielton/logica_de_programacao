45c1ef591d447566dc3b3ab205e9caba10eb0ae3
Ellielton <ellieltongama@gmail.com>
2023-10-28 18:41:13 -0300

    corrigindo bug.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = parseInt(Math.random()*10 + 1);
+let numeroSecreto = parseInt(Math.random()*100 + 1);
 console.log(numeroSecreto);
 
 //var chute armazena o que o usuário digitar no prompt.
@@ -8,7 +8,7 @@ let tentativa = 1
 
 //Enquanto o chut não for igual ao numero secreto faça:
 while (chute != numeroSecreto) {
-  chute = prompt('Escolha um número entre 1 e 10');
+  chute = prompt('Escolha um número entre 1 e 100');
   //condicional que compara se o chute é igual ao número secreto.
   if (chute == numeroSecreto) {
 
