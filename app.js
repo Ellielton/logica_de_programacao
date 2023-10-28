fb5f884ab3496ca8c0a9cbbf6311b1563cfb6745
Ellielton <ellieltongama@gmail.com>
2023-10-28 17:33:35 -0300

    Reescrevendo a estrutura if da aula anterior com operador ternário.

--- app.js
+++ app.js
@@ -28,12 +28,15 @@ while (chute != numeroSecreto) {
   }
   tentativa++;
 }
+//criando um operador ternário para melhorar a condicional if abaixo:
+
+//como se lê: Se tentativas é maior que 1? Se sim o valor é a string tentativas ou ":" se não o valor é string tentativa
+let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto} com ${tentativa} ${palavraTentativa}.`);
+
 //validado o alert no caso de tentativas ser maior que um pois a mensagem deve ser diferente se tentativas for = 1
-if(tentativa > 1) {
-
-  alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto} com ${tentativa} tentativas.`);
-
-} else {
-  alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto} com ${tentativa} tentativa.`);
-
-}
+// if(tentativa > 1) {
+//   alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto} com ${tentativa} tentativas.`);
+// } else {
+//   alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto} com ${tentativa} tentativa.`);
+// }
