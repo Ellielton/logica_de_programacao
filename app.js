8facb0a8154b11e834b98a8314d6d43d20fcb180
Ellielton <ellieltongama@gmail.com>
2023-12-16 19:08:13 -0300

    Corrigir todas as palavra da linha 2 acrescentando a letra a, reorganizei o comentário da linha 26

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroMximo = 100;
-let numeroSecreto = parseInt(Math.random() * numeroMximo + 1);
+let numeroMaximo = 10;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 
 //var chute armazena o que o usuário digitar no prompt.
@@ -9,7 +9,7 @@ let tentativa = 1
 
 //Enquanto o chut não for igual ao numero secreto faça:
 while (chute != numeroSecreto) {
-  chute = prompt(`Escolha um número entre 1 e ${numeroMximo}`);
+  chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
   //condicional que compara se o chute é igual ao número secreto.
   if (chute == numeroSecreto) {
     break;
@@ -23,14 +23,6 @@ while (chute != numeroSecreto) {
   }
   tentativa++;
 }
-//criando um operador ternário para melhorar a condicional if abaixo:
-
-//como se lê: Se tentativas é maior que 1? Se sim o valor é a string tentativas ou ":" se não o valor é string tentativa
+//criando um operador ternário para melhorar a condicional if abaixo: se lê: tentativas é maior que 1? Se sim o valor é atribuido a variavel o string antes dos ":" senão o que esttiver depois será o valor
 let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
 alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto} com ${tentativa} ${palavraTentativa}.`);
-//validado o alert no caso de tentativas ser maior que um pois a mensagem deve ser diferente se tentativas for = 1
-// if(tentativa > 1) {
-//   alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto} com ${tentativa} tentativas.`);
-// } else {
-//   alert(`Isso ai! Você descobriu o numero secreto é ${numeroSecreto} com ${tentativa} tentativa.`);
-// }
